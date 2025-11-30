71856b6a2dd15e12595e750bbbebbf39f75a9d9a
lemoineo <olivier.lemoine@grenoble-inp.fr>
2025-12-01 00:28:28 +0100

    affichage des prix

--- src/main/java/service/ServiceCatalogue.java
+++ src/main/java/service/ServiceCatalogue.java
@@ -8,16 +8,11 @@ import java.sql.Statement;
 
 public class ServiceCatalogue {
 
-     String cmd_sql = "SELECT idLot, idArticle, quantiteDisponible, " +
-            "TRUNC(datePeremption - SYSDATE) AS nbJourRestant " +
-            "FROM Lot " +
-            "WHERE quantiteDisponible <> 0 " +
-            "AND datePeremption < SYSDATE + 7";
     
     // --- AFFICHAGE (Catalogue/Alertes) ---
 
     private static void consulterCatalogue(Connection conn) {
-        String sql = "SELECT i.idItem, i.typeItem, p.nom, a.modeConditionnement, a.prixVenteClient, c.typeContenant, c.capacite, c.prixVente, " +
+        String sql = "SELECT i.idItem, i.typeItem, p.nom, a.modeConditionnement, a.prixVenteClient, c.typeContenant, c.capacite, c.prixVente, c.stock" +
                      "(SELECT SUM(quantiteDisponible) FROM Lot WHERE idArticle = a.idArticle) as stk " +
                      "FROM Item i LEFT JOIN Article a ON i.idArticle=a.idArticle LEFT JOIN Produit p ON a.idProduit=p.idProduit " +
                      "LEFT JOIN Contenant c ON i.idContenant=c.idContenant ORDER BY i.idItem";
@@ -25,8 +20,8 @@ public class ServiceCatalogue {
             System.out.println("--- CATALOGUE ---");
             while(rs.next()) {
                 String desc = "ARTICLE".equals(rs.getString("typeItem")) ? 
-                    rs.getString("nom") + " (" + rs.getString("modeConditionnement") + ") - Stock: " + rs.getInt("stk") :
-                    rs.getString("typeContenant") + " (" + rs.getDouble("capacite") + ")";
+                    rs.getString("nom") + " (" + rs.getString("modeConditionnement") + ") - Prix: " + rs.getFloat("prixVenteClient") + "€ - Stock: " + rs.getInt("stk") :
+                    rs.getString("typeContenant") + " (" + rs.getDouble("capacite") + ") - Prix: " + rs.getFloat("prixVente") + "€ - Stock: " + rs.getInt("stock");
                 System.out.printf("[%d] %s\n", rs.getInt("idItem"), desc);
             }
         } catch (SQLException e) { e.printStackTrace(); }
